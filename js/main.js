32341f996b43f394b21fe2bf941c2acd6b069d07
i-sak <isaac7263@naver.com>
2019-12-30 01:27:58 +0900

    mobile size application, mobile button add...

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// 리사이즈 이벤트리스너
+window.onresize = chkSize;
+function chkSize(e){
+    console.log(window.innerWidth);
+    // 브라우저 너비
+    var width = window.innerWidth;
+    var nav = document.getElementById("nav");
+    var mob_btn_div = document.getElementById("mob_btn_div");
+    // 900보다 작으면 안보이기
+    if (width <= 900) {
+        nav.style.display = "none";
+        mob_btn_div.style.display = "block";
+    } else {
+        // 크면 보이기
+        nav.style.display = "inline-block";
+        mob_btn_div.style.display = "none";
+    }
+}
