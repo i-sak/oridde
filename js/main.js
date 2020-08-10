cc069a63ba472dea476d6ab8d579e0aded416301
i-sak <isaac7263@naver.com>
2020-08-11 00:34:53 +0900

    footer 수정

--- js/main.js
+++ js/main.js
@@ -6,13 +6,17 @@ function chkSize(e){
     var width = window.innerWidth;
     var nav = document.getElementById("nav");
     var mob_btn_div = document.getElementById("mob_btn_div");
+    var box_in_foot = document.getElementById("box_in_foot");
     // 900보다 작으면 안보이기
     if (width <= 900) {
         nav.style.display = "none";
         mob_btn_div.style.display = "block";
+        box_in_foot.style.fontSize = "12px";
     } else {
         // 크면 보이기
         nav.style.display = "inline-block";
         mob_btn_div.style.display = "none";
     }
 }
+
+
